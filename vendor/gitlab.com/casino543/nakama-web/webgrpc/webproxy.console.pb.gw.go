2a286921a590818392580b03e8686f54d0dff8de
deflinhec <deflinhec@gmail.com>
2023-04-24 11:09:53 +0200

    kick account implementation

--- vendor/gitlab.com/casino543/nakama-web/webgrpc/webproxy.console.pb.gw.go
+++ vendor/gitlab.com/casino543/nakama-web/webgrpc/webproxy.console.pb.gw.go
@@ -32,7 +32,7 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
-func request_ConsoleProxy_SessionLogout_0(ctx context.Context, marshaler runtime.Marshaler, client ConsoleProxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_ConsoleProxy_KickAccount_0(ctx context.Context, marshaler runtime.Marshaler, client ConsoleProxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AccountId
 	var metadata runtime.ServerMetadata
 
@@ -53,12 +53,12 @@ func request_ConsoleProxy_SessionLogout_0(ctx context.Context, marshaler runtime
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
 	}
 
-	msg, err := client.SessionLogout(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.KickAccount(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_ConsoleProxy_SessionLogout_0(ctx context.Context, marshaler runtime.Marshaler, server ConsoleProxyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_ConsoleProxy_KickAccount_0(ctx context.Context, marshaler runtime.Marshaler, server ConsoleProxyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AccountId
 	var metadata runtime.ServerMetadata
 
@@ -79,7 +79,7 @@ func local_request_ConsoleProxy_SessionLogout_0(ctx context.Context, marshaler r
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
 	}
 
-	msg, err := server.SessionLogout(ctx, &protoReq)
+	msg, err := server.KickAccount(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -108,7 +108,7 @@ func local_request_ConsoleProxy_GetMailConfig_0(ctx context.Context, marshaler r
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterConsoleProxyHandlerFromEndpoint instead.
 func RegisterConsoleProxyHandlerServer(ctx context.Context, mux *runtime.ServeMux, server ConsoleProxyServer) error {
 
-	mux.Handle("POST", pattern_ConsoleProxy_SessionLogout_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_ConsoleProxy_KickAccount_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -116,12 +116,12 @@ func RegisterConsoleProxyHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/nakama.web.ConsoleProxy/SessionLogout", runtime.WithHTTPPathPattern("/v2/console/account/{id}/logout"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/nakama.web.ConsoleProxy/KickAccount", runtime.WithHTTPPathPattern("/v2/console/account/{id}/kick"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_ConsoleProxy_SessionLogout_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_ConsoleProxy_KickAccount_0(annotatedContext, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
@@ -129,7 +129,7 @@ func RegisterConsoleProxyHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			return
 		}
 
-		forward_ConsoleProxy_SessionLogout_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ConsoleProxy_KickAccount_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -199,25 +199,25 @@ func RegisterConsoleProxyHandler(ctx context.Context, mux *runtime.ServeMux, con
 // "ConsoleProxyClient" to call the correct interceptors.
 func RegisterConsoleProxyHandlerClient(ctx context.Context, mux *runtime.ServeMux, client ConsoleProxyClient) error {
 
-	mux.Handle("POST", pattern_ConsoleProxy_SessionLogout_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_ConsoleProxy_KickAccount_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/nakama.web.ConsoleProxy/SessionLogout", runtime.WithHTTPPathPattern("/v2/console/account/{id}/logout"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/nakama.web.ConsoleProxy/KickAccount", runtime.WithHTTPPathPattern("/v2/console/account/{id}/kick"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ConsoleProxy_SessionLogout_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ConsoleProxy_KickAccount_0(annotatedContext, inboundMarshaler, client, req, pathParams)
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_ConsoleProxy_SessionLogout_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ConsoleProxy_KickAccount_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -247,13 +247,13 @@ func RegisterConsoleProxyHandlerClient(ctx context.Context, mux *runtime.ServeMu
 }
 
 var (
-	pattern_ConsoleProxy_SessionLogout_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"v2", "console", "account", "id", "logout"}, ""))
+	pattern_ConsoleProxy_KickAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"v2", "console", "account", "id", "kick"}, ""))
 
 	pattern_ConsoleProxy_GetMailConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v2", "console", "config", "mail"}, ""))
 )
 
 var (
-	forward_ConsoleProxy_SessionLogout_0 = runtime.ForwardResponseMessage
+	forward_ConsoleProxy_KickAccount_0 = runtime.ForwardResponseMessage
 
 	forward_ConsoleProxy_GetMailConfig_0 = runtime.ForwardResponseMessage
 )
